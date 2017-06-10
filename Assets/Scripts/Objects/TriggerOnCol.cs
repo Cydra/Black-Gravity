bb3bd2e911b21fa947d08ebfe2829b8e2c867e48
Marcel <schulte.m.96@web.de>
2017-06-10 12:25:09 +0200

    Level 1_1 fertig gestellt

---
+++ Assets/Scripts/Objects/TriggerOnCol.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerOnCol : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ITriggerEvent[] events =  this.GetComponents<ITriggerEvent>();
+        foreach(ITriggerEvent myEvent in events)
+        {
+            myEvent.trigger();
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/TriggerScripts/moveOnce.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class moveOnce : MonoBehaviour, ITriggerEvent
+{
+    private bool activated = false;
+    public GameObject destination;
+    public Vector3 moveDir;
+    public float travelTime;
+    private float timePassed;
+
+	public void trigger()
+    {
+        this.activated = true;
+    }
+
+    void Update()
+    {
+        if (this.activated == true && this.timePassed < this.travelTime)
+        {
+            this.destination.transform.Translate(this.moveDir * (Time.deltaTime / this.travelTime), Space.World);
+            this.timePassed += Time.deltaTime;
+        }
+    }
+}
